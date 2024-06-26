e8c9fd32957d4a441e44f53fc3785accff72624c
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-06-26 17:02:23 +0000

    Pending changes exported from your codespace

---
+++ sentiment.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import re
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+#!pip install wordcloud
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+df = pd.read_csv('labeled_data.csv')
+
