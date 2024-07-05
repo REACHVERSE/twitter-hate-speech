5c9a2ad9eadeffb40cc6ea5d4c7529f821ad3989
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-05 23:30:05 +0000

    updated settings to allow vercel

--- hatespeech/hatemodel/urls.py
+++ hatespeech/hatemodel/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("predict/", views.PredictView.as_view()),
     path("predictions/", views.PredictionListAPIView.as_view()),
-    path("predictions/<int:pk>/delete/", views.PredictionDeleteView.as_view()),
     path("stats/", views.PredictionStatsView.as_view()),
     path("stats/graph/", views.PredictionCountByPredictionView.as_view()),
     path("reviews/post/", views.ReviewPostView.as_view()),
--- hatespeech/hatespeech/settings.py
+++ hatespeech/hatespeech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0$sfrfnttubqi0y#rox58u*opuqfvuffsx(hh^shu)8zm#vyvp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
