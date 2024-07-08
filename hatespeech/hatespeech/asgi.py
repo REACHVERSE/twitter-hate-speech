b82bd4c52b5426107862ab13b3ac9dda098ce4cb
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-08 20:33:56 +0000

    new file structure

--- hatespeech/hatespeech/asgi.py
+++ hatespeech/hatespeech/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatespeech.hatespeech.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatespeech.settings")
 
 application = get_asgi_application()
