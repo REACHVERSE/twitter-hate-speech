f4f02e9da6741bfa8320f54fe07b6ff556dbe33a
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-09 14:32:48 +0000

    render config default db changed :)

--- hatespeech/settings.py
+++ hatespeech/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/hatespeech',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
