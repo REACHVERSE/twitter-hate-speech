5d0c1e353aae1b7a862773283b95a3cb6d10c97e
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-09 00:46:19 +0000

    asgi import removed

--- hatespeech/asgi.py
+++ hatespeech/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-import hatemodel as hatemodel
+#import hatemodel as hatemodel
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatespeech.settings")
 
