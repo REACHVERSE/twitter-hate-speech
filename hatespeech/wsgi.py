eff35da86c3dc77d5fa895b262b1b6a7c2a7783f
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-09 01:46:34 +0000

    removed wsgi application

--- hatespeech/wsgi.py
+++ hatespeech/wsgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
 #import hatespeech.hatespeech.asgi
 #import hatemodel as hatemodel
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatespeech.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
-app = application
+#app = application
