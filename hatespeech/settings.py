9be57ccbc13393f932a112b5636dc8738976dfaa
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-09 02:18:55 +0000

    removed whitenoise

--- hatespeech/settings.py
+++ hatespeech/settings.py
@@ -132,22 +132,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 #STATIC_URL = "static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-if not DEBUG:
+"""if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-"""STORAGES = {
+STORAGES = {
     # ...
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
-}"""
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
