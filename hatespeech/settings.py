1adfc8d90bdb9ebc7af2a7c483abc21807c95096
Oritsemisan Omare <85939415+MisanOmar3@users.noreply.github.com>
2024-07-09 01:36:30 +0000

    removed wsgi from settings

--- hatespeech/settings.py
+++ hatespeech/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    #"whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "hatespeech.wsgi.application"
+#WSGI_APPLICATION = "hatespeech.wsgi.application"
 
 
 # Database
