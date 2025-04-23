9e5bd617d3f6b96efef3cfce660e488852e8576b
MisanOmar3 <misanomare09@gmail.com>
2025-04-23 16:49:27 +0100

    added logging

--- hatespeech/asgi.py
+++ hatespeech/asgi.py
@@ -8,11 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import logging
 from django.core.asgi import get_asgi_application
 
-#import hatemodel as hatemodel
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info("👋 ASGI application is initializing...")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatespeech.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hatespeech.settings')
 
 application = get_asgi_application()
+
+logger.info("✅ ASGI application is ready!")
